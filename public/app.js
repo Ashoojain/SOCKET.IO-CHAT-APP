3832586fb1c1267a68a21393e32b2a994bad7636
Ashoojain <ashoojain6@gmail.com>
2018-07-30 00:04:37 +0530

    chat

---
+++ public/app.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+    var socket = io();
+    let btn = $('#btn');
+    let inp = $('#inp');
+    let result = $('#result');
+
+
+    display();
+
+    btn.click(function(){
+        let value = inp.val();
+        socket.emit('message', value);
+    });
+
+    socket.on('show', function(data) {
+        result.append(`<li>${data}</li>`);
+
+    })
+
+    function render(data) {
+        data.forEach((msg)=>{
+            result.append(`<li>${msg}</li>`)
+        })
+
+    }
+
+    function display() {
+        socket.on('ms', function(data){
+            render(data);
+
+        })
+    }
+});
